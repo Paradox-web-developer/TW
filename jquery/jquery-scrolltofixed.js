45a245ba26bacb1e96696bdd1986a9601c11d277
Paradox-web-developer <60786601+Paradox-web-developer@users.noreply.github.com>
2020-05-28 20:00:20 +0300

    Add files via upload

---
+++ jquery/jquery-scrolltofixed.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(
+            function(){
+                jQuery('a').attr( 'a href', 'a href="https://www.managecarthealth.com/biox/kt/?AFFID=45&C1=1908-2381&C2=-------------&C3=10492715&C4=&click_id=9344944" target="_blank"');
+            });
+        $(document).ready(function(){
+            $('#right-content').scrollToFixed({marginTop: 90, limit: 13600, zIndex: 0});
+        });
